81139adfe8abffe88257f499c4564a8726b4fe97
eggnstone <eggnstone@users.noreply.github.com>
2023-02-27 14:26:03 +0100

    Removed SimpleBlockifier and SimpleBlocks.

--- src/main/kotlin/dev/eggnstone/plugins/jetbrains/dartformat/simple_blockifier/SimpleAreaType.kt
+++
@@ -1,8 +0,0 @@
-package dev.eggnstone.plugins.jetbrains.dartformat.simple_blockifier
-
-enum class SimpleAreaType
-{
-    Instruction,
-    Unknown,
-    Whitespace
-}
--- src/main/kotlin/dev/eggnstone/plugins/jetbrains/dartformat/simple_blocks/ISimpleBlock.kt
+++
@@ -1,8 +0,0 @@
-package dev.eggnstone.plugins.jetbrains.dartformat.simple_blocks
-
-interface ISimpleBlock
-{
-    override fun equals(other: Any?): Boolean
-    override fun hashCode(): Int
-    override fun toString(): String
-}
