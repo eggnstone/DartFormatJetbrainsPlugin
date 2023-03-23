11a357745696ec52f072eefd8d7f28154757659f
eggnstone <eggnstone@users.noreply.github.com>
2023-03-23 09:38:57 +0100

    Adding notifications.

--- src/main/kotlin/dev/eggnstone/plugins/jetbrains/dartformat/plugin/PluginFormat.kt
+++ src/main/kotlin/dev/eggnstone/plugins/jetbrains/dartformat/plugin/PluginFormat.kt
@@ -1,25 +1,36 @@
 package dev.eggnstone.plugins.jetbrains.dartformat.plugin
 
+import com.intellij.notification.*
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentIterator
+import com.intellij.openapi.ui.popup.Balloon
+import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.BalloonImpl
+import com.intellij.util.ui.JBUI
 import dev.eggnstone.plugins.jetbrains.dartformat.DartFormatException
 import dev.eggnstone.plugins.jetbrains.dartformat.config.DartFormatConfig
 import dev.eggnstone.plugins.jetbrains.dartformat.config.DartFormatPersistentStateComponent
 import dev.eggnstone.plugins.jetbrains.dartformat.dotlin.DotlinLogger
 import dev.eggnstone.plugins.jetbrains.dartformat.indenters.iIndenters.MasterIndenter
 import dev.eggnstone.plugins.jetbrains.dartformat.splitters.iSplitters.MasterSplitter
+import java.awt.Font
+import java.util.*
+import javax.swing.BoxLayout
+import javax.swing.JLabel
+import javax.swing.JPanel
 
-typealias FormatHandler = (virtualFile: VirtualFile, project: Project) -> Boolean
+typealias FormatHandler = (virtualFile: VirtualFile, project: Project, logs: MutableList<String>) -> Boolean
 
 class PluginFormat : AnAction()
 {
@@ -28,21 +39,26 @@ class PluginFormat : AnAction()
         private val masterSplitter = MasterSplitter()
     }
 
-    private class FormatIterator(private val format: FormatHandler, private val project: Project) : ContentIterator
+    private class FormatIterator(private val format: FormatHandler, private val project: Project, private val logs: MutableList<String>) : ContentIterator
     {
         override fun processFile(virtualFile: VirtualFile): Boolean
         {
             //DotlinLogger.log("FormatIterator.processFile: $virtualFile")
-            return format(virtualFile, project)
+            return format(virtualFile, project, logs)
         }
     }
 
     override fun actionPerformed(e: AnActionEvent)
     {
+        val startTime = Date()
+
+        val project = e.getRequiredData(CommonDataKeys.PROJECT)
+        val editor = e.getData(CommonDataKeys.EDITOR_EVEN_IF_INACTIVE)
+
         try
         {
-            val project = e.getRequiredData(CommonDataKeys.PROJECT)
-            val formatIterator = FormatIterator(this::formatDartFile, project)
+            val logs = mutableListOf<String>()
+            val formatIterator = FormatIterator(this::formatDartFile, project, logs)
             val virtualFiles = e.getRequiredData(CommonDataKeys.VIRTUAL_FILE_ARRAY)
 
             CommandProcessor.getInstance().runUndoTransparentAction {
@@ -53,10 +69,19 @@ class PluginFormat : AnAction()
                     VfsUtilCore.iterateChildrenRecursively(virtualFile, this::filterDartFiles, formatIterator)
                 }
             }
+
+            val endTime = Date()
+            val diffTime = endTime.time - startTime.time
+            logs.add(0, "Formatting ${virtualFiles.size} file${if (virtualFiles.size == 1) "" else "s"} took $diffTime ms.")
+
+            notify(project, editor, logs)
         }
         catch (err: DartFormatException)
         {
-            throw DartFormatException("While formatting: ${err.message}")
+            val errMessage = if (err.message == null) "?" else err.message!!
+            notify(project, editor, listOf("While formatting:", e.toString(), errMessage))
+            val message = "While formatting: $e\n${err.message}"
+            throw DartFormatException(message)
             /*
             DotlinLogger.log("While formatting: $e:")
             DotlinLogger.log("$err")
@@ -65,9 +90,59 @@ class PluginFormat : AnAction()
         }
         catch (err: AssertionError)
         {
+            notify(project, editor, listOf("While formatting:", e.toString(), err.toString()))
             DotlinLogger.log("While formatting: $e:")
             DotlinLogger.log("$err")
         }
+    }
+
+    private fun notify(project: Project, editor: Editor?, logs: List<String>)
+    {
+        notifyByToolWindowBalloon(project, logs)
+        notifyByEditorPopup(editor, logs)
+    }
+
+    private fun notifyByToolWindowBalloon(project: Project, logs: List<String>)
+    {
+        val combinedLogs = logs.joinToString("\n")
+
+        val notificationGroup = NotificationGroupManager.getInstance().getNotificationGroup("DartFormat Balloon Notifications")
+        val notification = notificationGroup.createNotification("DartFormat", combinedLogs, NotificationType.INFORMATION)
+        notification.notify(project)
+    }
+
+    private fun notifyByEditorPopup(editor: Editor?, logs: List<String>)
+    {
+        if (editor == null)
+        {
+            DotlinLogger.log("editor == null")
+            //val combinedLogs = logs.joinToString("\n")
+            //DotlinLogger.log(combinedLogs)
+            return
+        }
+
+        val factory = JBPopupFactory.getInstance()
+
+        val panel = JPanel()
+        panel.layout = BoxLayout(panel, BoxLayout.PAGE_AXIS)
+        panel.border = JBUI.Borders.empty(4)
+
+        val title = JLabel("DartFormat")
+        title.font = Font(title.font.name, Font.BOLD, 14)
+        title.border = JBUI.Borders.emptyBottom(4)
+        panel.add(title)
+
+        for (log in logs)
+            panel.add(JLabel(log))
+
+        val balloonBuilder = factory.createBalloonBuilder(panel)
+        val balloon = balloonBuilder.createBalloon()
+        val position = factory.guessBestPopupLocation(editor)
+
+        if (balloon is BalloonImpl)
+            balloon.setFillColor(panel.background)
+
+        balloon.show(position, Balloon.Position.below)
     }
 
     private fun filterDartFiles(virtualFile: VirtualFile): Boolean = virtualFile.isDirectory || isDartFile(virtualFile)
@@ -89,7 +164,7 @@ class PluginFormat : AnAction()
         return true
     }
 
-    private fun formatDartFile(virtualFile: VirtualFile, project: Project): Boolean
+    private fun formatDartFile(virtualFile: VirtualFile, project: Project, logs: MutableList<String>): Boolean
     {
         if (!isDartFile(virtualFile))
         {
@@ -105,7 +180,7 @@ class PluginFormat : AnAction()
             if (fileEditor == null)
                 return formatDartFileByBinaryContent(virtualFile)
 
-            return formatDartFileByFileEditor(fileEditor)
+            return formatDartFileByFileEditor(fileEditor, logs)
         }
         catch (err: DartFormatException)
         {
@@ -165,7 +240,7 @@ class PluginFormat : AnAction()
         }
     }
 
-    private fun formatDartFileByFileEditor(fileEditor: FileEditor): Boolean
+    private fun formatDartFileByFileEditor(fileEditor: FileEditor, logs: MutableList<String>): Boolean
     {
         if (fileEditor !is TextEditor)
         {
@@ -182,12 +257,16 @@ class PluginFormat : AnAction()
             val inputText = document.text
             val outputText = format(inputText)
             if (outputText == inputText)
+            {
+                logs += "Nothing changed."
                 return true
+            }
 
             ApplicationManager.getApplication().runWriteAction {
                 document.setText(outputText)
             }
 
+            logs += "Something changed."
             return true
         }
         catch (err: DartFormatException)
