a5a255b858f56bdfc2d618ef93ab7cf68dbab264
eggnstone <eggnstone@users.noreply.github.com>
2024-02-03 10:02:20 +0700

    v2.0.2: Fixed plugin verification problem.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("java")
     id("org.jetbrains.intellij") version "1.13.2"
+    //id("org.jetbrains.intellij") version "1.17.0" => Unsupported class file major version 63
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
 }
 
 group = "dev.eggnstone.plugins.jetbrains"
-version = "2.0.1"
+version = "2.0.2"
 
 repositories {
     mavenCentral()
@@ -19,6 +20,11 @@ dependencies {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2022.1.4")
+
+    //version.set("2023.3.3")
+    // Module was compiled with an incompatible version of Kotlin.
+    // The binary version of its metadata is 1.9.0, expected version is 1.7.1.
+
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
--- src/main/kotlin/dev/eggnstone/plugins/jetbrains/dartformat/plugin/FormatJob.kt
+++ src/main/kotlin/dev/eggnstone/plugins/jetbrains/dartformat/plugin/FormatJob.kt
@@ -1,9 +1,17 @@
 package dev.eggnstone.plugins.jetbrains.dartformat.plugin
 
-import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.Job
 
-class FormatJob(val command: String, val config: String?, val inputText: String?, val fileName: String?) : CompletableJob by Job()
+// : CompletableJob by Job() => plugin verification error: Abstract method Job.getParent() is not implemented
+class FormatJob(val command: String, val config: String?, val inputText: String?, val fileName: String?)
 {
+    private val job = Job()
+
     var formatResult: FormatResult? = null
+
+    fun cancel() = job.cancel()
+
+    fun complete() = job.complete()
+
+    suspend fun join() = job.join()
 }
