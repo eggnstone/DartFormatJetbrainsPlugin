24592a07157de01b80d4a65e144ed653a6a0c238
eggnstone <eggnstone@users.noreply.github.com>
2024-02-14 15:31:47 +0700

    Disabled timeout on startup.
    (cherry picked from commit 5f629ee6f08f4b6a92a412b1b94d105d6659327d)

--- src/main/kotlin/dev/eggnstone/plugins/jetbrains/dartformat/Constants.kt
+++ src/main/kotlin/dev/eggnstone/plugins/jetbrains/dartformat/Constants.kt
@@ -13,7 +13,7 @@ class Constants
 
         const val WAIT_INTERVAL_IN_MILLIS = 100
         const val WAIT_FOR_FORMAT_IN_SECONDS = 10
-        const val WAIT_FOR_EXTERNAL_DART_FORMAT_START_IN_SECONDS = 60
+        const val WAIT_FOR_EXTERNAL_DART_FORMAT_START_IN_SECONDS = -1 //60
         const val WAIT_FOR_EXTERNAL_DART_FORMAT_RESPONSE_IN_SECONDS = 5
     }
 }
