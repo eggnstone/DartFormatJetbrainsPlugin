43231d4fd3b8baffbb0291356ab9f01b4840b9b2
eggnstone <eggnstone@users.noreply.github.com>
2024-07-27 20:16:50 +0200

    Reduced and improved logging.

--- src/main/kotlin/dev/eggnstone/plugins/jetbrains/dartformat/plugin/ProjectActivity.kt
+++ src/main/kotlin/dev/eggnstone/plugins/jetbrains/dartformat/plugin/ProjectActivity.kt
@@ -1,6 +1,7 @@
 package dev.eggnstone.plugins.jetbrains.dartformat.plugin
 
 import com.intellij.openapi.project.Project
+import dev.eggnstone.plugins.jetbrains.dartformat.Constants
 import dev.eggnstone.plugins.jetbrains.dartformat.tools.Logger
 
 class ProjectActivity : com.intellij.openapi.startup.ProjectActivity
